66d8cc7a5a2b1878261e289c7c23ff4fc4380d5b
Evgeny <buriy-anarchist@inbox.ru>
2022-07-18 18:25:23 +0300

    Add task 36

---
+++ task_36/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] matrix = new int[10];
+for (int i = 0; i < matrix.Length; i++)
+{
+    Console.Write($"Введите matrix[{i}] = ");
+    matrix[i] = int.Parse(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < matrix.Length; i++)
+{
+    if (matrix[i] % 2 == 1)
+        sum += matrix[i];
+}
+Console.WriteLine($"Сумма нечетных чисел = {sum}");
