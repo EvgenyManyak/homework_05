ffb6d9d0d21e7bae8e125eede085ba859d4bf0fb
Evgeny <buriy-anarchist@inbox.ru>
2022-07-18 18:04:19 +0300

    Add task 34

---
+++ task_34/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] matrix = new int[8];
+
+void FillArray(int[] matrix, int min, int max){
+  for (int i = 0; i < matrix.Length; i++ )
+  {
+    matrix[i] = new Random().Next(min, max);
+  }
+}
+
+void PrintArray(int[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++ )
+    {
+    Console.Write(matrix[i] + " ");
+    }
+  Console.WriteLine();
+}
+
+int CountNumbers(int[] matrix)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.Length; i++ )
+    {
+    if (matrix[i] % 2 == 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+FillArray(matrix, 100, 1000);
+PrintArray(matrix);
+Console.WriteLine();
+
+int count = CountNumbers(matrix);
+Console.WriteLine($"Количество чётных чисел: {count}");
